8d415116832f448bfeb245636acb124b168d422a
Apos-dot-net <kumaradityasr@gmail.com>
2022-02-05 09:25:41 +0530

    validation (field is required) working BETTER

--- Admin/js/main.js
+++ Admin/js/main.js
@@ -67,43 +67,146 @@ Array.from(document.getElementsByClassName('field')).forEach(function (el) {
 
 /*Validation*/
 function errorHelp(el, str){
-  try{
-    const temp = el.closest(("."+el.tagName.toLowerCase()).toString());
-    const tempHelp = temp.closest('.field').querySelector('.help');
-    if(str){
-      temp.classList.add('is-danger')
-      tempHelp.innerHTML = str
-      tempHelp.classList.remove('is-hidden');
-    }else if(!str && !temp.classList.contains('is-hidden')){
-      temp.classList.remove('is-danger')
-      tempHelp.classList.add('is-hidden')
-    }
-  }catch (all){
-
-  }finally {
-    const temp = el.closest('.control').firstElementChild;
-    const tempHelp = temp.closest('.field').parentElement.querySelector('.help');
-    console.log(temp.closest('.field'))
-    if(str){
-      temp.classList.add('is-danger')
-      tempHelp.innerHTML = str
-      tempHelp.classList.remove('is-hidden');
-    }else if(!str && !temp.classList.contains('is-hidden')){
-      temp.classList.remove('is-danger')
-      tempHelp.classList.add('is-hidden')
-    }
+  const help = el.querySelector('.help')
+  if(str) {
+    help.innerHTML = str
+    help.classList.remove('is-hidden');
+  } else {
+    help.classList.add('is-hidden');
   }
 }
+
+function validateOptions(name) {
+  const runner= document.getElementsByName(name);
+
+  for (let i = 0; i < runner.length; i++) {
+    if (runner[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const ping = (url, timeout = 6000) => {
+  return new Promise((resolve, reject) => {
+    const urlRule = new RegExp('(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]');
+    if (!urlRule.test(url)) reject('invalid url');
+    try {
+      fetch(url)
+        .then(() => resolve(true))
+        .catch(() => resolve(false));
+      setTimeout(() => {
+        resolve(false);
+      }, timeout);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 // noinspection DuplicatedCode
 Array.from(document.getElementsByTagName("form")).forEach(function(el) {
   el.addEventListener("submit",function(ela) {
-    ela.preventDefault();
-    Array.from(el.getElementsByClassName("is-danger-passive"), async function(e) {
-        if (!e.value && !e.classList.contains("is-danger") && !e.disabled) {
-          await errorHelp(e, 'This field is required')
-        } else if((e.value || e.disabled) && e.classList.contains("is-danger")){
-          await errorHelp(e);
+      ela.preventDefault();
+      Array.from(el.getElementsByClassName("is-danger-passive"), async function(e) {
+
+        if(e.type === "text"){
+          if(!e.value && !e.disabled){
+            e.classList.add('is-danger');
+            errorHelp(e.closest(".control"), 'This Field is Required');
+            e.classList.add('apply-shake');
+          }else if(e.value && e.classList.contains('is-danger')){
+            e.classList.remove('is-danger');
+            errorHelp(e.closest(".control"));
+          }
+          await e.addEventListener('animationend', function (){
+            e.classList.remove('apply-shake');
+          })
+
+        } else if(e.type === "radio"){
+          if(validateOptions(e.name) && e.classList.contains("is-danger")){
+            e.classList.remove("is-danger");
+            errorHelp(e.closest(".control"));
+          } else if(!validateOptions(e.name)){
+            e.classList.add("is-danger");
+            e.closest(".field").classList.add("apply-shake");
+            errorHelp(e.closest(".control"), 'Choose an Option');
+          }
+          await e.closest(".field").addEventListener('animationend', function (){
+            e.closest(".field").classList.remove('apply-shake');
+          })
+
+        } else if(e.type === "date"){
+          if(!e.value){
+            e.classList.add("is-danger");
+            e.classList.add("apply-shake");
+            errorHelp(e.closest(".control"), 'This Field is Required')
+          }else if(e.value && e.classList.contains("is-danger")){
+            e.classList.remove("is-danger");
+            errorHelp(e.closest(".control"));
+          }
+          await e.addEventListener('animationend', function (){
+            e.classList.remove('apply-shake');
+          })
+
+        } else if(e.type === "file"){
+          if(!e.value){
+            e.classList.add("is-danger");
+            e.previousElementSibling.classList.add("is-danger");
+            e.previousElementSibling.classList.add("apply-shake");
+            errorHelp(e.closest(".field").parentElement, 'This Field is Required')
+          }else if(e.value && e.classList.contains("is-danger")){
+            e.classList.remove("is-danger");
+            e.previousElementSibling.classList.remove("is-danger");
+            errorHelp(e.closest(".field").parentElement);
+          }
+          await e.previousElementSibling.addEventListener('animationend', function (){
+            e.previousElementSibling.classList.remove('apply-shake');
+          })
+
+        } else if(e.type === "number"){
+          if(!e.value && !e.disabled){
+            e.classList.add("is-danger");
+            e.classList.add("apply-shake");
+            errorHelp(e.closest(".control"), 'This Field is Required')
+          }else if((e.value || e.disabled) && e.classList.contains("is-danger")){
+            e.classList.remove("is-danger");
+            errorHelp(e.closest(".control"));
+          }
+          await e.addEventListener('animationend', function (){
+            e.classList.remove('apply-shake');
+          })
+
+        } else if(e.tagName === "SELECT"){
+          if(!e.value){
+            e.classList.add("is-danger");
+            e.closest(".select").classList.add("is-danger");
+            e.closest(".select").classList.add("apply-shake");
+            errorHelp(e.closest(".control"), 'This Field is Required')
+          }else if(e.value && e.classList.contains("is-danger")){
+            e.classList.remove("is-danger");
+            e.closest(".select").classList.remove("is-danger");
+            errorHelp(e.closest(".control"));
+          }
+          await e.closest(".select").addEventListener('animationend', function (){
+            e.closest(".select").classList.remove('apply-shake');
+          })
+        } else if(e.type === "email"){
+          if(!e.value && !e.disabled){
+            e.classList.add('is-danger');
+            errorHelp(e.closest(".control"), 'This Field is Required');
+            e.classList.add('apply-shake');
+          }else if(e.value){
+            console.log(e.value.substring(e.value.indexOf('@') + 1))
+            e.classList.remove('is-danger');
+            errorHelp(e.closest(".control"));
+          }
+          await e.addEventListener('animationend', function (){
+            e.classList.remove('apply-shake');
+          })
+
         }
+
       })
     }
   )
