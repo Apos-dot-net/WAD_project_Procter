f0e52a2813daac5ebed7d366dbeb4dd089378757
Apos-dot-net <kumaradityasr@gmail.com>
2022-02-09 20:48:54 +0530

    profile moved to general examples named to records

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
-const {window} = new JSDOM();
+
+// const {window} = new JSDOM();
 const {document} = (new JSDOM('')).window;
 const express = require('express');
+const path = require("path");
 const app = express();
 
 global.document = document;
-app.use(express.static("admin"))
-app.use('/Admin/img', express.static('images'))
+app.use(express.static("Admin"));
+app.use(express.static(path.join("/img", "Admin")));
 
-const $ = jQuery = require('jquery')(window);
-
-app.listen(3001)
-
+app.listen(3001);
